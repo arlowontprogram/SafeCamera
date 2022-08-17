7a730570c7357b3a27f38781fb3f94b983bc5bf3
arlo <38384052+arlowontprogram@users.noreply.github.com>
2022-08-18 00:12:13 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,69 @@
+-- @ metadata
+-- @ title: PlayerCameraController
+-- @ creator: 110029109
+-- @ github: https://github.com/arlowontprogram/SafeCamera
+-- @ version: 1.0
+
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local PlayerCameraModule = {}
+PlayerCameraModule.__index = PlayerCameraModule
+
+local function UpdateCam(Cam, Part)
+	pcall(function()
+		Cam.CameraType = Enum.CameraType.Scriptable
+		Cam.CFrame = Part.CFrame
+	end)
+end
+
+function PlayerCameraModule:SetCamera(Camera)
+	if not Camera then
+		warn("Camera not defined")
+		return self
+	end
+	self.Camera = Camera
+end
+
+function PlayerCameraModule:Close()
+	if self.Heartbeat ~= nil then
+		self.Heartbeat:Disconnect()
+	end
+	if self.Destroying ~= nil then
+		self.Destroying:Disconnect()
+	end
+	self.Camera.CameraType = Enum.CameraType.Custom
+	self.Camera.CameraSubject = self.CameraSubject
+end
+
+function PlayerCameraModule:ChangeCamera(Basepart: BasePart): boolean
+	-- checks
+	if not Basepart then
+		warn("Object", Basepart, "not defined.")
+		return false
+	end
+	if not Basepart:IsA("BasePart") then
+		warn("Object", Basepart, "is not a BasePart")
+		return false
+	end
+	-- save subject
+	self.CameraSubject = self.Camera.CameraSubject
+	-- cut heartbeat
+	if self.Heartbeat ~= nil then
+		self.Heartbeat:Disconnect()
+	end
+	-- listen for part removal
+	-- we cant listen to Destroying since ancestry changes to nil won't call Destroying
+	--self.Destroying = Basepart.Destroying:Connect(function()
+	
+	self.Destroying = Basepart.AncestryChanged:Connect(function()
+		if not Basepart:IsDescendantOf(game) then
+			self:Close()
+		end
+	end)
+	-- create new heartbeat instance
+	self.Heartbeat = RunService.Heartbeat:Connect(function()
+		UpdateCam(self.Camera, Basepart)
+	end)
+end
+
+return setmetatable({}, PlayerCameraModule)
